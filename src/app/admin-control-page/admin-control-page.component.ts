9a86f111f65ec299d993d625151fd68c2d3fa251
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-02-19 00:41:19 +0200

    routes setup

---
+++ src/app/admin-control-page/admin-control-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-control-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-control-page.component.html',
+  styleUrl: './admin-control-page.component.css'
+})
+export class AdminControlPageComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { CategoriesAdminTableComponent } from './categories-admin-table/categories-admin-table.component';
+import { BooksAdminTableComponent } from './books-admin-table/books-admin-table.component';
+import { AuthorsAdminTableComponent } from './authors-admin-table/authors-admin-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //   {
+  //     path: 'adminLogin',
+  //     component: AdminLoginComponent,
+  //     title: 'Admin Login',
+  //   },
+  {
+    path: 'categoriesAdminTable',
+    component: CategoriesAdminTableComponent,
+    title: 'categories Admin Table',
+  },
+  {
+    path: 'booksAdminTable',
+    component: BooksAdminTableComponent,
+    title: 'Books Admin Table',
+  },
+  {
+    path: 'authorsAdminTable',
+    component: AuthorsAdminTableComponent,
+    title: 'Authors Admin Table',
+  },
+  //   {
+  //     path: '**',
+  //     component: NotFoundPage,
+  //   },
+];
