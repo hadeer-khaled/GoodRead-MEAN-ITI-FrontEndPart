53c8ba0ada9b291b8a873d24cdb65308c7b456e4
unknown <menna.abdallh11@gmail.com>
2024-02-20 11:48:26 +0200

    first delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // import { AdminLoginComponent } from './pages/admin-login/admin-login.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-
+// import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { DeleteConfirmComponent } from './components/popUpComponents/delete-confirm/delete-confirm.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet ,/* AdminLoginComponent */ NavBarComponent],
+  imports: [RouterOutlet , DeleteConfirmComponent /* AdminLoginComponent,NavBarComponent*/],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
