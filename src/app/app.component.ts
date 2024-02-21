c142e7575ad29792ef043960bf795f61cea95bad
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-02-21 21:30:35 +0200

    solve nav bar conflict

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AdminLoginComponent } from './pages/admin-login/admin-login.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { DeleteConfirmComponent } from './components/delete-confirm/delete-confirm.component';
+import { NavBarComponent } from './components/mainPageComponents/nav-bar/nav-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    DeleteConfirmComponent, AdminLoginComponent,
-    NavBarComponent
+    DeleteConfirmComponent,
+    AdminLoginComponent,
+    NavBarComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -3,11 +3,17 @@ import { Component } from '@angular/core';
 import { LandingComponent } from '../../components/landing/landing.component';
 import { NavBarComponent } from '../../components/mainPageComponents/nav-bar/nav-bar.component';
 import { PopularBooksComponent } from '../../components/mainPageComponents/popular-books/popular-books.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [NavBarComponent, LandingComponent, PopularBooksComponent],
+  imports: [
+    NavBarComponent,
+    LandingComponent,
+    PopularBooksComponent,
+    FooterComponent,
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css',
 })
