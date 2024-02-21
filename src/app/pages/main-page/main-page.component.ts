b7ff8de5c5deab0f3b0fdc97c86cadfc260891d4
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-02-21 21:06:08 +0200

    some of the main page

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { LandingComponent } from '../../components/landing/landing.component';
 import { NavBarComponent } from '../../components/mainPageComponents/nav-bar/nav-bar.component';
+import { PopularBooksComponent } from '../../components/mainPageComponents/popular-books/popular-books.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [NavBarComponent, LandingComponent],
+  imports: [NavBarComponent, LandingComponent, PopularBooksComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css',
 })
